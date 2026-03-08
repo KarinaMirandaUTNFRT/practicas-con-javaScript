ef50c1b9b230568870dfc4be30e7062370c76118
Karina Miranda UTN <Karina.Miranda@frt.utn.edu.ar>
2026-03-08 17:28:11 -0300

    rama para sumar dos numeros

---
+++ java/Ejercicios/02-SumaDosNumeros.js
@@ -0,0 +1,4 @@
+const num1 = prompt("Ingresa el primer numero:")//abre una ventana para agregar un mensaje
+const num2 = prompt("Ingresa el segundo numero:")//abre una ventana para agregar un mensaje
+console.log(num1)
+console.log(num2)
