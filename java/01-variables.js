283af25b5d8d21b9cf677dfb03199b17e967a60c
Karina Miranda UTN <Karina.Miranda@frt.utn.edu.ar>
2026-03-08 11:39:38 -0300

    tipos de variables

---
+++ java/01-variables.js
@@ -0,0 +1,38 @@
+//Esto es un comentario simple solo de esta linea
+//sigue el comentario
+// ej de definir variable let y const
+let nombre = "Maria";
+const iva = 1.21;
+const url = "https://universe.rollingcodeschool.com/home";
+let anio = 2026; //number
+let precio = 20.57; //number
+/*Let puede cambiar en el tiempo*/
+console.log("Holaaaa Mundo");
+nombre = "juan ";
+console.log(nombre);
+console.log(iva);
+console.log("Contenido de la variable nombre:", nombre);
+console.log("Contenido de la variable nombre:" + nombre);
+console.log(url);
+console.log(anio);
+console.log(precio);
+
+
+//Si quiero guardar textos
+const apellido = "Navarro"; //string
+//number
+const negativo = -100; //number
+//variable vacia
+const vacio = null
+let dato //no se sabe que tipo de datos voy a guardar
+//datos booleano
+let encendido = true
+let apagado = false
+
+//despues de crear muestro
+console.log(apellido)
+console.log(negativo)
+console.log(vacio)
+console.log(dato) //como no sabe que dato muestra indefinido
+dato = 1234
+console.log(dato)
