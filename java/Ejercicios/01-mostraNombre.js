ee8e50d610909a74d101d1a003cf08c59289ccdd
Karina Miranda UTN <Karina.Miranda@frt.utn.edu.ar>
2026-03-08 18:34:17 -0300

    se agrego document.close

--- java/Ejercicios/01-mostraNombre.js
+++ java/Ejercicios/01-mostraNombre.js
@@ -1,9 +1,12 @@
-const nombre = prompt("Ingresa tu nombre:")//abre una ventana para agregar un mensaje
-const apellido = prompt("Ingresa tu apellido:")//abre una ventana para agregar un mensaje
-console.log(nombre)
-console.log(apellido)
+const nombre = prompt("Ingresa tu nombre:"); //abre una ventana para agregar un mensaje
+const apellido = prompt("Ingresa tu apellido:"); //abre una ventana para agregar un mensaje
+console.log(nombre);
+console.log(apellido);
 
 //mostrar un mensaje al usuario
 //alert("Tu nombre y Apellido es: " + nombre + ',' + apellido )
 //document.writeln('<p> Tu nombre y Apellido es : ' + nombre + ' ' + apellido + ' </p>'  )
-document.writeln(`<p> Tu nombre y Apellido es :  ${nombre}  ${apellido}   </p> `)
+document.writeln(
+  `<p> Tu nombre y Apellido es :  ${nombre}  ${apellido}   </p> `,
+);
+document.close();
