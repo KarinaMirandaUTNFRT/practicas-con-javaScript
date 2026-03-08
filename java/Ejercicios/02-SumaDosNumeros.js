6d85249b9c945b007c436888a6d35bf902891907
Karina Miranda UTN <Karina.Miranda@frt.utn.edu.ar>
2026-03-08 18:08:12 -0300

    Muestra por consola la ventana de ingreso para cada valor num1 y num2

--- java/Ejercicios/02-SumaDosNumeros.js
+++ java/Ejercicios/02-SumaDosNumeros.js
@@ -1,5 +1,5 @@
-let num1 = parseInt( prompt("Ingresa el primer numero:"))//abre una ventana para agregar un mensaje
-let num2 = parseInt(  prompt("Ingresa el segundo numero:"))//abre una ventana para agregar un mensaje
+let num1 = prompt("Ingresa el primer numero:")//abre una ventana para agregar un mensaje
+let num2 = prompt("Ingresa el segundo numero:")//abre una ventana para agregar un mensae
 console.log(num1)
 console.log(num2)
-document.writeln(`<p> El resultado de sumar  ${num1}  +  ${num2}  es: ${(num1 + num2)} </p>`);
+
