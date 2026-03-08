f85c65032697529b355b49b22a9423d10947ce05
Karina Miranda UTN <Karina.Miranda@frt.utn.edu.ar>
2026-03-08 18:09:07 -0300

    SE realiza la operacion de suma

--- java/Ejercicios/02-SumaDosNumeros.js
+++ java/Ejercicios/02-SumaDosNumeros.js
@@ -2,4 +2,4 @@ let num1 = prompt("Ingresa el primer numero:")//abre una ventana para agregar un
 let num2 = prompt("Ingresa el segundo numero:")//abre una ventana para agregar un mensae
 console.log(num1)
 console.log(num2)
-
+document.writeln(`<p> El resultado de sumar  ${num1}  +  ${num2}  es: ${(num1 + num2)} </p>`);
