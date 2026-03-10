96ed5fe39053ccc45bfcb4ac745c022484beb5e7
Karina Miranda UTN <Karina.Miranda@frt.utn.edu.ar>
2026-03-10 10:06:31 -0300

    comienza el ejercicio de par/impar

---
+++ java/Ejercicios/02-SumaDosNumeros.js
@@ -0,0 +1,5 @@
+let num1 = parseInt(prompt("Ingresa el primer numero:"))//abre una ventana para agregar un mensaje
+let num2 = parseInt(prompt("Ingresa el segundo numero:"))//abre una ventana para agregar un mensae
+console.log(num1)
+console.log(num2)
+document.writeln(`<p> El resultado de sumar  ${num1}  +  ${num2}  es: ${(num1 + num2)} </p>`);
---
+++ java/Ejercicios/02-condicionales.js
@@ -0,0 +1,17 @@
+//determinar si un numero es par o impar
+//el usuario ingresar un número y determinar si es par o impar. Mostrar el resultado.
+//Algoritmo Par_impar
+	//definir numero1 Como Entero
+	//escribir "Ingresa tu numero:"
+	//leer numero1
+	//si numero1 % 2 =0 Entonces
+		//escribir"el numero",numero1," es par"
+	//sino 
+		//escribir"el numero",numero1," es impar"
+	//FinSi
+//FinAlgoritmo
+// creo las variables
+const numero1 = parseInt(prompt('Ingresa un numero para comparar')) 
+console.log(numero1)
+console.log(20)
+console.log('hola')
