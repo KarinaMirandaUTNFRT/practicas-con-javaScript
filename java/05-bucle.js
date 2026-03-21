a7548d5d987c0628d819c354380eeccffcd5759f
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-21 19:48:08 -0300

    cambios en condicionales

--- java/05-bucle.js
+++ java/05-bucle.js
@@ -1,22 +1,44 @@
-
-//while- do while - for
-/* const pantalla = document.getElementById("pantalla");
-let contador = 1;
-
-while (contador <= 50){   //la condicion debe cumplirse al principio
-    pantalla.innerHtml += `<p> Fila ${contador}</p>`
-}
-
+//cree un programa al estilo de un cajero automatico :
+//1-consulta
+//2-Ingresar dinero
+//3-extraer dinero
 do {
-    pantalla.innerHtml += `<p> Fila ${contador}</p>`
-    contador ++; //contador = contador +1
-} while (contador <= 50);
+  const opcion = prompt(
+    "Íngresa una opcion: 1-consultar el saldo /2 -ingresar dinero /3-extraer dinero",
+  );
 
+  let saldo = 1000; //ambito global, por que la uso desde la linea 5 en adelante
 
-for (let contador = 1; contador <=50; contador ++ ){
-pantalla.innerHTML += '<p> Fila ${contador}</p>'
-} */
-
-while(confirm('queres realizar esta operacion')){
-    pantalla.innerHTML += '<p> Fila Creada con confirm </p>'
-}
+  switch (opcion) {
+    case "saldo":
+    case "1":
+    case 1:
+      alert("Tu saldo actual es $" + saldo);
+      break;
+    case "deposito":
+    case "2":
+    case 2:
+      const deposito = parseInt(
+        prompt("Ingresa el monto que deseas depositar"),
+      );
+      saldo = saldo + deposito;
+      alert(`Ingresaste $ ${deposito}, tu saldo actual es $${saldo}`);
+      break;
+    case "3":
+    case "extraer":
+    case "3":
+    case 3:
+      const extraccion = parseInt(prompt("ingresa el monto a extraer"));
+      if (extraccion <= saldo) {
+        saldo = saldo - extraccion;
+        alert(`Retiraste $${extraccion}, tu saldo actual es $${saldo}`);
+      } else {
+        alert("Fondos insuficientes");
+      }
+      break;
+    default:
+      //aqui agrego todas las lineas de codigo que necesito si la opcion ingresada por el ususario es 3
+      alert("Ingresaste una opcion incorrecta");
+  }
+} while (confirm("queres realizar otra operacion?"));
+console.log("Aqui termino la ejecucion del switch");
