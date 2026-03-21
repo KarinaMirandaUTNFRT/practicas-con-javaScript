798317389a469077bc745b3623fb6fd4168483d1
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-21 15:55:37 -0300

    cambios en condicionales

--- java/02-condicionales.js
+++ java/02-condicionales.js
@@ -1,30 +1,44 @@
-//determinar si un numero es par o impar
-//el usuario ingresar un número y determinar si es par o impar. Mostrar el resultado.
-//Algoritmo Par_impar
-//definir numero1 Como Entero
-//escribir "Ingresa tu numero:"
-//leer numero1
-//si numero1 % 2 =0 Entonces
-//escribir"el numero",numero1," es par"
-//sino
-//escribir"el numero",numero1," es impar"
-//FinSi
-//FinAlgoritmo
-// creo las variables
-const numero1 = parseInt(prompt("Ingresa un numero para comparar"));
-console.log(numero1);
-console.log(isNaN(numero1));
-console.log(isNaN(4));
+//cree un programa al estilo de un cajero automatico :
+//1-consulta
+//2-Ingresar dinero
+//3-extraer dinero
+do {
+  const opcion = prompt(
+    "Íngresa una opcion: 1-consultar el saldo /2 -ingresar dinero /3-extraer dinero",
+  );
 
-//validamos que el usuario ingrese un numero no un string
-if (isNaN(numero1)) {
-  alert("Ingresaste un valor erroneo");
-} else {
-  const total = numero1 % 2;
-  console.log(total);
-if (total === 0)
-  alert("El numero es par");
-} else {
-  alert("El numero es impar");
-}
-}
+  let saldo = 1000; //ambito global, por que la uso desde la linea 5 en adelante
+
+  switch (opcion) {
+    case "saldo":
+    case "1":
+    case 1:
+      alert("Tu saldo actual es $" + saldo);
+      break;
+    case "deposito":
+    case "2":
+    case 2:
+      const deposito = parseInt(
+        prompt("Ingresa el monto que deseas depositar"),
+      );
+      saldo = saldo + deposito;
+      alert(`Ingresaste $ ${deposito}, tu saldo actual es $${saldo}`);
+      break;
+    case "3":
+    case "extraer":
+    case "3":
+    case 3:
+      const extraccion = parseInt(prompt("ingresa el monto a extraer"));
+      if (extraccion <= saldo) {
+        saldo = saldo - extraccion;
+        alert(`Retiraste $${extraccion}, tu saldo actual es $${saldo}`);
+      } else {
+        alert("Fondos insuficientes");
+      }
+      break;
+    default:
+      //aqui agrego todas las lineas de codigo que necesito si la opcion ingresada por el ususario es 3
+      alert("Ingresaste una opcion incorrecta");
+  }
+} while (confirm("queres realizar otra operacion?"));
+console.log("Aqui termino la ejecucion del switch");
